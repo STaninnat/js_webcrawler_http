78ac322e80a47c95aa659db7c13eb1ba105b9703
STaninnat <taninnat.s@gmailcom>
2024-06-09 02:22:43 +0700

    complete web crawler http

---
+++ report.test.js
@@ -0,0 +1,31 @@
+const {sortPages} = require('./report.js')
+const {test, expect} = require('@jest/globals')
+
+test('sortPages 2 pages', () => {
+    const input = {
+        'http://wagslane.dev/path' : 1,
+        'http://wagslane.dev' : 3
+    }
+    const actual = sortPages(input)
+    const expected = [['http://wagslane.dev', 3], ['http://wagslane.dev/path', 1]]
+    expect(actual).toEqual(expected)
+})
+
+test('sortPages 5 pages', () => {
+    const input = {
+        'http://wagslane.dev/path' : 1,
+        'http://wagslane.dev/path4' : 5,
+        'http://wagslane.dev/path2' : 8,
+        'http://wagslane.dev/path3' : 2,
+        'http://wagslane.dev' : 3
+    }
+    const actual = sortPages(input)
+    const expected = [
+        ['http://wagslane.dev/path2', 8],
+        ['http://wagslane.dev/path4', 5],
+        ['http://wagslane.dev', 3], 
+        ['http://wagslane.dev/path3', 2],
+        ['http://wagslane.dev/path', 1]
+    ]
+    expect(actual).toEqual(expected)
+})
